39df19089a781791fb062d39dba655e6c9f64f04
Frank <codingfrank73@gmail.com>
2022-02-12 14:18:11 +0100

    fehlerhafte Anzeige nach änderung von Steuersatz und Berechnungsverfahren gefixt

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,13 @@ function changeHeadlines(checkedId, outputText) {
     document.getElementById("outputText").innerHTML = outputText;
 }
 
-function clearInAndOut() {
-    document.getElementById("inputValue").value = '';
-    document.getElementById("vatValue").innerHTML = '';
-    document.getElementById("outputValue").innerHTML = '';
+function setDefaultValues() {
+    document.getElementById("vatValue").innerHTML = '0,00 €';
+    document.getElementById("outputValue").innerHTML = '0,00 €';
+}
+
+function validateInput(inputValue) {
+    inputValue <= 0 ? setDefaultValues() : setCalcValues(inputValue);
 }
 
 //Hilfsfunktion um zu prüfen ob ein das Element ausgewählt ist.
@@ -14,10 +17,12 @@ function isChecked(elementId) {
     return document.getElementById(elementId).checked == true ? true : false;
 }
 
+//Ermittelt den Steuersatz mit den gerechnet wird.
 function getVatRate() {
     return isChecked("nineteen") ? 1.19 : 1.07;
 }
 
+//Gibt die ermittelten Werte nach der Berechnung an die UI-Elemente zurück.
 function setCalcValues(inputValue) {
     let result = calc(inputValue);
 
@@ -25,21 +30,22 @@ function setCalcValues(inputValue) {
     document.getElementById("vatValue").innerHTML = vat(inputValue, result).toFixed(2) + ` €`;
 }
 
+//Ermittelt welche Berechnungverfahren durchgeführt werden soll und startet dies.
 function calc(inputValue) {
     return isChecked("calcBrutto") ? nettoToBrutto(getVatRate(), inputValue) : bruttoToNetto(getVatRate(), inputValue);
 }
 
-// Von einem Bruttobetrag wird der Nettobetrag berechnet
+// Von einem Bruttobetrag wird der Nettobetrag berechnet.
 function bruttoToNetto(vatRate, inputValue) {
     return inputValue / vatRate;
 }
 
-// Aus dem Nettobetrag wird der Bruttobetrag berechnet
+// Aus dem Nettobetrag wird der Bruttobetrag berechnet.
 function nettoToBrutto(vatRate, inputValue) {
     return inputValue * vatRate;
 }
 
-//Steuerbetrag berechnen
+//Steuerbetrag berechnen.
 function vat(inputValue, result) {
     let x = inputValue - result;
     return (x < 0) ? x * -1 : x;
